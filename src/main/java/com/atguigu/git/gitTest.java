03f25eb3ed4a85bf2e9f1bd810b22e7fe5cf51b8
wen136841202 <136841202@qq.com>
2021-08-21 20:45:44 +0800

    the second commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -3,7 +3,7 @@ package com.atguigu.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello test");
-        System.out.println("the second test");
+        System.out.println("the first test");
         System.out.println("the second test");
     }
 }
